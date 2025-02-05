78d09dad81aaeef4e2f7c4b338491ba5991033dc
rickmareshazamme <99163920+rickmareshazamme@users.noreply.github.com>
2025-02-05 14:19:52 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(){}).call(this);
+
+}
+/*
+     FILE ARCHIVED ON 12:34:32 Jan 26, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:15:36 Feb 05, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.718
+  exclusion.robots: 0.027
+  exclusion.robots.policy: 0.012
+  esindex: 0.014
+  cdx.remote: 20.143
+  LoadShardBlock: 76.274 (3)
+  PetaboxLoader3.datanode: 92.013 (4)
+  load_resource: 172.575
+  PetaboxLoader3.resolve: 101.963
+*/
